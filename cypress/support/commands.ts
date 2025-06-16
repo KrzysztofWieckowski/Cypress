0875ff158425b560fa5a312d5bed7a7596651821
Krzysztof <krzyszwiec@wp.pl>
2025-06-16 22:13:10 +0200

    Correction 5

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,7 +2,6 @@
 /**
  * This command allows to open the home page and assert the url easily.
  */
-// @ts-ignore
 Cypress.Commands.add('openHomePage', ()=>{
     cy.visit('/')
     cy.url().should('eq', 'https://automationintesting.online/')
